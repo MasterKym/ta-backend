3ae6a42c6ea6a90ba237722e4dbd66f2c09804b2
Mohamed Yassir Khalid <git@atkyms.com>
2022-05-14 15:58:24 +0100

    logger middleware

---
+++ src/middleware/logger.middleware.ts
@@ -0,0 +1,8 @@
+import { Request, Response, NextFunction } from 'express';
+
+const logger = async (req:Request, res:Response, next:NextFunction) => {
+  // console.log(`${new Date().toLocaleTimeString()} -- ${req.method} to '${req.originalUrl}'`)
+  console.log(`${req.method} '${req.originalUrl}' -- ${new Date().toLocaleTimeString()}`);
+  next();
+};
+export default logger;

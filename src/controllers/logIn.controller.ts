ff9ef01bc2e3f0203f8c20667df46a07dfaa9f59
Mohamed Yassir Khalid <git@atkyms.com>
2022-05-17 16:59:25 +0100

    add jwt logic

--- src/controllers/logIn.controller.ts
+++ src/controllers/logIn.controller.ts
@@ -2,6 +2,8 @@ import { Request, Response } from 'express';
 import prisma from '../prisma/prisma';
 import bcrypt from 'bcrypt';
 import Joi from 'joi';
+import JWT_SECRET from '../env/JWT_SECRET';
+import jwt from 'jsonwebtoken';
 
 const logIn = async (req:Request, res:Response) => {
 
@@ -25,6 +27,21 @@ const logIn = async (req:Request, res:Response) => {
   }
   const match = await bcrypt.compare(password, user.password);
   if(!match) return res.status(401).json({ message:"Wrong password" });
+  
+  
+  // generate token
+  const token = jwt.sign(
+    {
+      username,
+    },
+    JWT_SECRET,
+    { expiresIn: "1h" }
+  );
+  console.log(token);
+
+  // put JWT in cookie
+  res.cookie("login-token", token);
+
   return res.status(200).json({message: "Successful login !"});
 };
 

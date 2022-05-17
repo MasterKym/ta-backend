937c8b21849c26df07420217c4c54024739b588b
Mohamed Yassir Khalid <git@atkyms.com>
2022-05-17 17:00:15 +0100

    add profile route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,10 +2,13 @@ import { Express } from 'express';
 import checkAPIHealth from '../controllers/checkAPIHealth.controller';
 import signUp from '../controllers/signUp.controller';
 import logIn from '../controllers/logIn.controller';
+import checkLogin from '../middleware/checkLogin.middleware';
+import profile from '../controllers/profile.controller'
 
 const routes = (app: Express) => {
-  app.route('/').get(checkAPIHealth);
+  // app.route('/').get(checkAPIHealth);
   app.route('/signup').post(signUp);
   app.route('/login').post(logIn);
+  app.route('/profile').get(checkLogin, profile);
 };
 export default routes;

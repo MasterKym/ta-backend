f8937955f5ea9ddee6155313b6d8ab12155c023c
Mohamed Yassir Khalid <git@atkyms.com>
2022-05-17 16:59:04 +0100

    add cookie-parser and path module

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import path from 'path';
 import express from 'express';
 import routes from './routes/index';
 import logger from './middleware/logger.middleware';
+import cookieParser from "cookie-parser";
 
 const app = express();
 const port = process.env.PORT || 4000;
@@ -8,6 +10,8 @@ const port = process.env.PORT || 4000;
 // I think middleware should go before the routes
 app.use(express.json());
 app.use(logger);
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
 // add the listenets for all routes
 routes(app);

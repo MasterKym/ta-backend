f9396c2d969d54b01ce62dea32cb9dc5c3cfe01f
Mohamed Yassir Khalid <git@atkyms.com>
2022-06-18 19:48:34 +0100

    update getProfile function

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -2,11 +2,14 @@
 
 import { NextFunction, Request, Response } from 'express';
 
-const profile = async (req:Request, res:Response, next:NextFunction) => {
+export const getProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const { username, role } = res.locals.user;
 
-  return res.status(200)
-  .send(`Hello ${username}. Your role is: <b>${role}</b>`);
+  return res
+    .status(200)
+    .send(`Hello ${username}. Your role is: <b>${role}</b>`);
 };
-
-export default profile;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,12 +3,12 @@ import checkAPIHealth from '../controllers/checkAPIHealth.controller';
 import signUp from '../controllers/signUp.controller';
 import logIn from '../controllers/logIn.controller';
 import checkLogin from '../middleware/checkLogin.middleware';
-import profile from '../controllers/profile.controller'
+import { getProfile } from '../controllers/profile.controller';
 
 const routes = (app: Express) => {
   // app.route('/').get(checkAPIHealth);
   app.route('/signup').post(signUp);
   app.route('/login').post(logIn);
-  app.route('/profile').get(checkLogin, profile);
+  app.route('/profile').get(checkLogin, getProfile);
 };
 export default routes;

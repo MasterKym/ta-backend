575974f2168af1cc0ad39fa78878f658e68f14ed
ElayadeIsmail <elayadeismail@gmail.com>
2022-06-21 20:04:39 +0100

    Added update profile and reset password endpointds

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,14 +1,19 @@
 import { Express } from 'express';
-import checkAPIHealth from '../controllers/checkAPIHealth.controller';
-import signUp from '../controllers/signUp.controller';
 import logIn from '../controllers/logIn.controller';
+import {
+    changePassword,
+    getProfile,
+    updateProfile,
+} from '../controllers/profile.controller';
+import signUp from '../controllers/signUp.controller';
 import checkLogin from '../middleware/checkLogin.middleware';
-import { getProfile } from '../controllers/profile.controller';
 
 const routes = (app: Express) => {
-  // app.route('/').get(checkAPIHealth);
-  app.route('/signup').post(signUp);
-  app.route('/login').post(logIn);
-  app.route('/profile').get(checkLogin, getProfile);
+    // app.route('/').get(checkAPIHealth);
+    app.route('/signup').post(signUp);
+    app.route('/login').post(logIn);
+    app.route('/profile').get(checkLogin, getProfile);
+    app.route('/profile').put(checkLogin, updateProfile);
+    app.route('/change-password').post(checkLogin, changePassword);
 };
 export default routes;

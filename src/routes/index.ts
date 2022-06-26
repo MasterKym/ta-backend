049f71629fec0afe823c85ba1891696113829eeb
ElayadeIsmail <elayadeismail@gmail.com>
2022-06-26 14:15:16 +0100

    rm unused console.log

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,11 @@
 import { Express } from 'express';
 import logIn from '../controllers/logIn.controller';
-import signOut from '../controllers/signout.controller';
 import {
     changePassword,
     getProfile,
     updateProfile,
 } from '../controllers/profile.controller';
+import signOut from '../controllers/signout.controller';
 import signUp from '../controllers/signUp.controller';
 import checkLogin from '../middleware/checkLogin.middleware';
 
@@ -13,8 +13,9 @@ const routes = (app: Express) => {
     // app.route('/').get(checkAPIHealth);
     app.route('/signup').post(signUp);
     app.route('/login').post(logIn);
-    app.route('/profile').get(checkLogin, getProfile);
-    app.route('/profile').put(checkLogin, updateProfile);
+    app.route('/profile')
+        .get(checkLogin, getProfile)
+        .put(checkLogin, updateProfile);
     app.route('/change-password').post(checkLogin, changePassword);
     app.route('/signout').post(checkLogin, signOut);
 };

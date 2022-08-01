4c3914be480914eb8786c8b4dba0b33bc56dafaa
medelmredi <medelmredi@gmail.com>
2022-08-01 19:44:44 +0100

    Revert "update username"
    This reverts commit 4190b496487ca6015eabb675867c097554b6c8da.

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -63,7 +63,6 @@ export const updateProfile = async (req: Request, res: Response) => {
     email,
     dateOfBirth,
     phone,
-    username: newUserName
   }: {
     password: string;
     role: Role;
@@ -99,16 +98,7 @@ export const updateProfile = async (req: Request, res: Response) => {
       message: 'Password is incorrect',
     });
   }
-  const usernameTaken = await prisma.user.findUnique({
-    where: {
-      username: newUserName,
-    },
-  });
-  if (usernameTaken) {
-    return res.status(400).json({
-      message: 'Username is taken',
-    });
-  }
+
   await prisma.user
     .update({
       where: {
@@ -118,7 +108,6 @@ export const updateProfile = async (req: Request, res: Response) => {
         firstName,
         lastName,
         email,
-        username: newUserName,	
         password: hashedPassword,
         dateOfBirth: new Date(dateOfBirth),
         phone,

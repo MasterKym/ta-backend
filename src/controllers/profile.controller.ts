29870a0f35f428d7bf7460bd92f9d0cc28b97805
achaayb <achaayb@sekera-group.com>
2022-08-12 22:33:10 +0100

    add updateProfile method

--- src/controllers/profile.controller.ts
+++ src/controllers/profile.controller.ts
@@ -1,15 +1,81 @@
 
 
 import { NextFunction, Request, Response } from 'express';
+import prisma from "../prisma/prisma";
+import Joi from "joi";
+
 
 export const getProfile = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  const { username, role } = res.locals.user;
+  const { username } = res.locals.user;
+
+  const user = await prisma.user.findUnique({
+    where : {
+      username: username
+    }, select : {
+      firstName: true,
+      lastName: true,
+      email: true,
+      dateOfBirth: true,
+      phone: true
+
+    }
+  })
 
   return res
     .status(200)
-    .send(`Hello ${username}. Your role is: <b>${role}</b>`);
+    .json(user)
 };
+
+
+export const updateProfile = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+
+  const schema = Joi.object({
+    firstName: Joi.string().min(2).max(20),
+    lastName: Joi.string().min(2).max(20),
+    email: Joi.string().email(),
+    password: Joi.string().min(8).max(20),
+    dateOfBirth: Joi.date(),
+    phone: Joi.string().min(10).max(15),
+  }).min(1);
+
+  const { value, error } = schema.validate(req.body);
+  if (error) {
+    return res.status(400).json({
+        message: "The values you entered do not meet the requirements.",
+        error: error,
+      })
+  }
+  const { username } = res.locals.user;
+  const {
+    firstName,
+    lastName,
+    email,
+    password,
+    dateOfBirth,
+    phone
+  } = req.body
+
+  const updateUser = await prisma.user.update({
+    where: {
+      username: username,
+    },
+    data: {
+      firstName: firstName,
+      lastName: lastName,
+      email: email,
+      password: password,
+      dateOfBirth: dateOfBirth,
+      phone: phone
+    }
+  })
+
+  return res.status(204)
+}
